b05baeaeda9e17b480acc5e3841b97d28a3982ac
dylan <dylan@192.168.56.1>
2024-08-26 03:40:27 -0300

    1.0

---
+++ src/main/java/Spotify.java
@@ -0,0 +1,20 @@
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import views.VentanaPrincipal;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+/**
+ *
+ * @author dylan
+ */
+public class Spotify {
+
+    public static void main(String[] args) {
+        VentanaPrincipal v = new VentanaPrincipal();
+        v.setVisible(true);
+    }
+}
